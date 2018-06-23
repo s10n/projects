47e386aa7457d95062228eb958ce8b7af324df81
심철환 <a@akaiv.com>
2018-06-23 22:43:17 +0900

    패키지: Firestore 선언

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { db } from './firebase'
 import './index.css'
 import App from './components/App'
 
-ReactDOM.render(<App />, document.getElementById('projects'))
+ReactDOM.render(<App db={db} />, document.getElementById('projects'))
